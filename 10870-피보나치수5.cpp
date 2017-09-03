9970d1ce6829121ba8c37a2d85f4329e077c77e4
hsh <hans12312@naver.com>
2017-09-03 12:10:28 +0900

    7/3
    7/3

---
+++ 10870-피보나치수5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int fibo(int num){
+    if(num==0)  return 0;
+    else if(num==1) return 1;
+    else return fibo(num-1)+fibo(num-2);
+}
+
+int main(){
+    int number;
+    cin>>number;
+    cout<<fibo(number);
+
+}
